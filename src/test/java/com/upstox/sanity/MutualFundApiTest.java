f8cc827d70c3c4fc0c311075279a6337f7e43129
Anand Sajankar <anand.sajankar@upstox.com>
2026-04-24 20:27:50 +0530

    Merge pull request #58 from upstox/sync/mutual-funds-api-20260424
    Add mutual funds API endpoints, models, tests, and documentation

---
+++ src/test/java/com/upstox/sanity/MutualFundApiTest.java
@@ -0,0 +1,54 @@
+package com.upstox.sanity;
+
+import com.upstox.ApiClient;
+import com.upstox.ApiException;
+import com.upstox.Configuration;
+import com.upstox.api.GetMutualFundHoldingsResponse;
+import com.upstox.api.GetMutualFundOrderDetailsResponse;
+import com.upstox.api.GetMutualFundOrdersResponse;
+import com.upstox.api.GetMutualFundSipsResponse;
+import com.upstox.auth.OAuth;
+import io.swagger.client.api.MutualFundApi;
+
+public class MutualFundApiTest {
+    public static void main(String[] args) {
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
+
+        OAuth OAUTH2 = (OAuth) defaultClient.getAuthentication("OAUTH2");
+        OAUTH2.setAccessToken(DataToken.accessToken);
+
+        MutualFundApi apiInstance = new MutualFundApi();
+
+        try {
+            GetMutualFundHoldingsResponse result = apiInstance.getMutualFundHoldings();
+            System.out.println(result);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling MutualFundApi#getMutualFundHoldings");
+            System.out.println(e.getResponseBody());
+        }
+
+        try {
+            GetMutualFundOrdersResponse result = apiInstance.getMutualFundOrders(null, null, 1, 10);
+            System.out.println(result);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling MutualFundApi#getMutualFundOrders");
+            System.out.println(e.getResponseBody());
+        }
+
+        try {
+            GetMutualFundOrderDetailsResponse result = apiInstance.getMutualFundOrder("2TC3XY-7ef4a026-908c-4d56-8ab7-e4a517938083");
+            System.out.println(result);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling MutualFundApi#getMutualFundOrder");
+            System.out.println(e.getResponseBody());
+        }
+
+        try {
+            GetMutualFundSipsResponse result = apiInstance.getMutualFundSips(1, 10);
+            System.out.println(result);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling MutualFundApi#getMutualFundSips");
+            System.out.println(e.getResponseBody());
+        }
+    }
+}
